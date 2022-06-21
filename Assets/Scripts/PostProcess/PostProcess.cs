a86eaad3dde817e23c5184c5b7ef94dbbc8a2805
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-06-22 01:05:50 +0800

    Implement material list for post process script

---
+++ Assets/Scripts/PostProcess/PostProcess.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcess : MonoBehaviour {
+	Camera targetCam;
+	Camera renderingCam;
+
+	void Start() {
+		targetCam = GetComponent<Camera>();
+		var renderingCamObj = new GameObject("Rendering Camera");
+		renderingCamObj.transform.parent = transform;
+		renderingCam = renderingCamObj.AddComponent<Camera>();
+		CameraController.CreateOn(renderingCamObj).target = targetCam;
+	}
+
+	static RenderTexture CreateRenderTexture(RenderTexture reference) {
+		if(reference != null)
+			return new RenderTexture(reference);
+		return RenderTexture.GetTemporary(Screen.width, Screen.height, 24);
+	}
+
+	RenderTexture buffer;
+	public List<Material> materials;
+
+	void OnPreRender() {
+		buffer = CreateRenderTexture(targetCam.targetTexture);
+		renderingCam.targetTexture = buffer;
+		renderingCam.Render();
+	}
+	
+	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		foreach(var mat in materials)
+			Graphics.Blit(buffer, buffer, mat);
+		Graphics.Blit(buffer, destination);
+		buffer.Release();
+	}
+}
